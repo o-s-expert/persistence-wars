a908dacfa899f88e5edee1521fc22804390d11cb
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-30 08:34:42 +0300

    feat: add player layer
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/expert/os/performance/App.java
+++ src/main/java/expert/os/performance/App.java
@@ -1,2 +1,24 @@
-package expert.os.performance;public class App {
+package expert.os.performance;
+
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
+import jakarta.nosql.document.DocumentTemplate;
+
+import java.util.Optional;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+            DocumentTemplate template = container.select(DocumentTemplate.class).get();
+
+            Player player = new Player("otavio", "Otavio Santana");
+
+            template.insert(player);
+
+            Optional<Player> otavio = template.find(Player.class, "otavio");
+            System.out.println("the entity " + otavio);
+        }
+    }
 }
--- src/main/java/expert/os/performance/Player.java
+++ src/main/java/expert/os/performance/Player.java
@@ -1,2 +1,24 @@
-package expert.os.performance;public class Player {
+package expert.os.performance;
+
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+@Entity
+public class Player {
+
+    @Id
+    private String nickname;
+
+    @Column
+    private String name;
+
+    public Player(String nickname, String name) {
+        this.nickname = nickname;
+        this.name = name;
+    }
+
+    Player() {
+    }
 }
